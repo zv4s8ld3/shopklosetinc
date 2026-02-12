01bfdf8e322d3daadd72f2c42b23272be561c595
Flora <141000543+zv4s8ld3@users.noreply.github.com>
2026-02-12 11:42:10 +0900

    Update astro.config.mjs
    Change Code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  output: 'server',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  adapter: vercel()
 });
